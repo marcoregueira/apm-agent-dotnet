3d108188b066c7e2a1d14a1af02052a8ad0767d1
Marco Regueira <marcoregueira@gmail.com>
2020-11-06 15:22:36 +0100

    Activar Timescale

---
+++ Windows.Metrics.Ingest/Migrations/PostgresDatabase/20201106141428_EnableTimescale.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Windows.Metrics.Ingest.Migrations.PostgresDatabase
+{
+    public partial class EnableTimescale : Migration
+    {
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			CustomMigration.EnableTimescaleDbIfAvailable(migrationBuilder);
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			// No way. If Timescaledb is enabled, drop the database and start again
+		}
+	}
+}
